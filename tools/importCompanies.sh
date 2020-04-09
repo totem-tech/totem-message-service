d254b382ea51806aa6090ec3f6b37f73d9ddf597
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-04-09 02:01:35 +0200

    Companies import process improvements

--- tools/importCompanies.sh
+++
@@ -1,8 +0,0 @@
-NODE_OPTIONS="--max-old-space-size=8192" \
-filepath="${1:-}" \
-startingNumber="1" \
-seed="//Alice${2:-}" \
-DBName="companies_test" \
-CouchDB_URL="http://admin:123456@127.0.0.1:5984" \
-CouchDB_BulkSize="10" \
-yarn run tools-company
