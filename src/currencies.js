f0f1fd37e28363bd3ea3fd1f2ec590f612361efc
Toufiqur Chowdhury <htr.letun@gmail.com>
2020-04-27 08:07:44 +0200

    CRUD

--- src/currencies.js
+++ src/currencies.js
@@ -14,7 +14,7 @@ const getAll = async () => await currencies.getAll(null, true, 99999)
 setTimeout(async () => {
     // create an index for the field `currency`, ignores if already exists
     const indexDef = {
-        index: { fields: [ 'currency' ] },
+        index: { fields: ['currency'] },
         name: 'currency-index',
     }
     const tickers = Array.from(await getAll()).map(([_, x]) => x.currency).sort()
@@ -35,24 +35,24 @@ export const handleCurrencyConvert = async (from, to, amount, callback) => {
     if (!isFn(callback)) return
     if (!isStr(from) || !from || !isStr(to) || !to || !amount || !isValidNumber(amount))
         return callback(messages.invalidRequest)
-        
+
     const fromCurrency = await currencies.find({
         currency: from.toUpperCase(),
     }, true, true, false)
     if (!fromCurrency) return callback(`${messages.notFound}: ${from}`)
-    
+
     const toCurrency = await currencies.find({
         currency: to.toUpperCase(),
     }, true, true, false)
     if (!toCurrency) return callback(`${messages.notFound}: ${to}`)
-    
+
     const convertedAmount = (fromCurrency.ratioOfExchange / toCurrency.ratioOfExchange) * amount
     callback(null, convertedAmount)
 }
 
 // retrives list of supported currency names
 export const handleCurrencyList = async (tickersArrHash, callback) => {
-    if(!isFn(callback)) return
+    if (!isFn(callback)) return
     // whether or not client needs to update the list of tickers
     const shouldUpdate = tickersArrHash !== tickersHash
     callback(null, !shouldUpdate ? new Map() : await getAll())
