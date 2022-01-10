9260e1b88ad3d9fe797faa31a64b751e738ba4c6
Toufiqur Chowdhury <htr.letun@gmail.com>
2022-01-10 21:48:55 +0300

    glAcccounts: fixed minor bug caused when non-array accountNumbers received

--- src/glAccounts.js
+++ src/glAccounts.js
@@ -1,5 +1,6 @@
 import CouchDBStorage from './utils/CouchDBStorage'
 import { isFn } from './utils/utils'
+import { TYPES, validate, validateObj } from './utils/validator'
 
 const fields = [
     'number',
@@ -17,9 +18,33 @@ const fields = [
 ]
 const glAccounts = new CouchDBStorage(null, 'gl-accounts', fields)
 
+/**
+ * @name    handleGlAccounts
+ * @summary fetch Global Ledger accounts by account numbers
+ * 
+ * @param   {Array}     accountNumbers 
+ * @param   {Function}  callback        callback arguments:
+ *                                      - error         String: error message if any
+ *                                      - glAccounts    Array
+ */
 export const handleGlAccounts = async (accountNumbers = [], callback) => {
     if (!isFn(callback)) return
-    if (!accountNumbers.length) return callback(null, new Map())
+
+    const err = validateObj(
+        { accountNumbers },
+        {
+            accountNumbers: {
+                min: 1,
+                max: 9999,
+                required: true,
+                type: TYPES.number,
+            }
+        },
+        true,
+        true,
+    )
+    if (err) return callback(err)
+
     const selector = {
         number: { $in: accountNumbers }
     }
